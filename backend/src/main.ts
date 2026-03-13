ad600af653a04b06af22bdff584b0d991d103841
darskp <darshanpatgar100@gmail.com>
2026-03-13 22:41:14 +0530

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-  import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     //means the pipe will apply to all controllers and routes globally.
     new ValidationPipe(
@@ -17,10 +17,23 @@ const app = await NestFactory.create(AppModule);
     )
   )
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api');
 
-mongoose.connection.once('open', () => {
-  console.log("MongoDB connected successfully 🚀");
-});
+  const allowedOrigin = process.env.ALLOWED_ORIGIN || 'http://localhost:3001';
+
+  app.enableCors({
+    origin: allowedOrigin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Backend is running on: http://localhost:${port}/api`);
+
+
+  mongoose.connection.once('open', () => {
+    console.log("MongoDB connected successfully 🚀");
+  });
 }
 bootstrap();
